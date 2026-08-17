c6f75b35051fbaad617b581a2727d0e0872ff2c3
vettr <vitorlimamoreira01@gmail.com>
2026-08-17 20:47:24 -0300

    Metodo getConnection ok na classe DB

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,12 +2,30 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
 
-    private static Properties loadProperties(){
-        try(FileInputStream fs = new FileInputStream("db.properties")) {
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        if (conn == null) {
+            try {
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+        return conn;
+    }
+
+    private static Properties loadProperties() {
+        try (FileInputStream fs = new FileInputStream("db.properties")) {
             Properties props = new Properties();
             props.load(fs);
             return props;
@@ -15,5 +33,4 @@ public class DB {
             throw new DbException(e.getMessage());
         }
     }
-
 }
