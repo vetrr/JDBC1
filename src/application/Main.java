f8d4db699c9125ab25ef5d2fcf6339916060b2c0
Vitor <vitorlimamoreira01@gmail.com>
2026-09-04 10:18:22 -0300

    Métodos para fechar o ResultSet e o Statement na classe DB

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,13 +7,7 @@ import java.sql.Connection;
 public class Main {
     static void main(String[] args) {
         Connection conn = DB.getConnection();
-
-        if (conn != null){
-            System.out.println("Conexao Estabelecida");
-        }
-        else {
-            System.out.println("Conexao Nao estabelecida");
-        }
+        conn = DB.closeConnection();
 
     }
 
--- src/db/DB.java
+++ src/db/DB.java
@@ -2,9 +2,7 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class DB {
@@ -24,7 +22,7 @@ public class DB {
         return conn;
     }
 
-    public Connection closeConnection(){
+    public static Connection closeConnection(){
         if(conn != null){
             try {
                 conn.close();
@@ -44,4 +42,25 @@ public class DB {
             throw new DbException(e.getMessage());
         }
     }
+
+    public static void closeStatement(Statement st){
+        if (st != null){
+            try {
+                st.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
+    public static void closeResultSet(ResultSet rs){
+        if (rs != null){
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+        }
+    }
+
 }
