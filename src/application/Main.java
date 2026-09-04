09c2bf327149e4f867fdd00c54f21943614d7f24
Vitor <vitorlimamoreira01@gmail.com>
2026-09-04 12:58:20 -0300

    Realizando a primeira consulta SQL (classe main).

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,34 @@
 package application;
 
 import db.DB;
+import db.DbException;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     static void main(String[] args) {
-        Connection conn = DB.getConnection();
-        conn = DB.closeConnection();
+        Connection conn = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        try {
+            conn = DB.getConnection();
+            st = conn.createStatement();
+            rs = st.executeQuery("select * from department");
+
+            while (rs.next()){
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        }finally {
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
 
     }
 
